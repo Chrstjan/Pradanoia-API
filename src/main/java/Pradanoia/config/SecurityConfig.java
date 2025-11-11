f1c17b2e7fb9dc8570774656947651bf8fc4692c
Chrstjan <ps5christian@hotmail.com>
2025-11-11 11:04:05 +0100

    Made most of the setup for JWT Auth for route access

---
+++ src/main/java/Pradanoia/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+﻿package Pradanoia.config;
+
+import Pradanoia.filters.JwtAuthFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDetailsService userDetailsService) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userDetailsService = userDetailsService;
+    }
+}
---
+++ src/main/java/Pradanoia/services/UserInfoService.java
@@ -0,0 +1,50 @@
+﻿package Pradanoia.services;
+
+import Pradanoia.entities.UserInfo;
+import Pradanoia.repositories.UserInfoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+    private final UserInfoRepository repository;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public UserInfoService(UserInfoRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    //Loads user details by username (email)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userInfo = repository.findByEmail(username);
+
+        if (userInfo.isEmpty())
+            throw new UsernameNotFoundException("User with email: " + username + " was not found");
+
+        //Converts into user entity model
+        UserInfo user = userInfo.get();
+        return new User(user.getEmail(), user.getPassword(),
+                user.getRoles());
+    }
+
+    // #region user registering methods
+    public String addUser(UserInfo userInfo) {
+        //Encrypts password before saving
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User added successfully!";
+    }
+    // #endregion user registering methods
+}
