207cf8e1482f577cb7b11f68dd66292a905c8ee0
Chrstjan <ps5christian@hotmail.com>
2025-11-17 15:07:38 +0100

    Start of comments controller

--- src/main/java/Pradanoia/PradanoiaApplication.java
+++ src/main/java/Pradanoia/PradanoiaApplication.java
@@ -1,13 +1,39 @@
 package Pradanoia;
 
+import Pradanoia.repositories.RoleRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class PradanoiaApplication {
+public class PradanoiaApplication implements CommandLineRunner {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleRepo roleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PradanoiaApplication.class, args);
 	}
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            System.out.println("Auto run on terminal args");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/Pradanoia/controllers/CommentController.java
@@ -0,0 +1,65 @@
+﻿package Pradanoia.controllers;
+
+import Pradanoia.config.AppConstants;
+import Pradanoia.payloads.entities.CommentDto;
+import Pradanoia.payloads.entities.PostResponse;
+import Pradanoia.payloads.utils.ApiResponse;
+import Pradanoia.services.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable Integer id) {
+        CommentDto comment = this.commentService.getCommentById(id);
+        return new ResponseEntity<>(comment, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}/comments")
+    public ResponseEntity<List<CommentDto>> getCommentsByUser(@PathVariable Integer userId) {
+        List<CommentDto> comments = this.commentService.getCommentsByUsers(userId);
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<PostResponse> getCommentsByPost(
+            @PathVariable Integer postId,
+            @RequestParam(value = "pageNum", defaultValue = AppConstants.PAGE_NUM, required = false) Integer pageNum,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize
+    ) {
+        PostResponse res = this.commentService.getCommentsByPost(postId, pageNum, pageSize);
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/user/{userId}/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@Valid @RequestBody CommentDto commentDto, @PathVariable Integer userId, @PathVariable Integer postId) {
+        CommentDto savedComment = this.commentService.createComment(commentDto, userId, postId);
+        return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@Valid @RequestBody CommentDto commentDto, @PathVariable Integer id) {
+        CommentDto updatedComment = this.commentService.updateComment(commentDto, id);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<?> deleteComment(@PathVariable Integer id) {
+        commentService.deleteComment(id);
+        return new ResponseEntity<>(new ApiResponse("Comment deleted successfully", true), HttpStatus.OK);
+    }
+}
